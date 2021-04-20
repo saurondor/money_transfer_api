a0777e07bf5933f48c032025214b2b1e9f2296ec
Gerardo Tasistro <gtasistro@tiempometa.com>
2021-04-20 17:08:10 -0500

    stub controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,28 @@
 Rails.application.routes.draw do
+
+
+  ## STUB CRUD money transfer endpoints
+  # we'll need to take into consideration
+  # - transfer out CLABE
+  #
+  namespace :api do
+    namespace :v1 do
+      get 'money_transfers' => "money_transfer#index"
+      get 'money_transfers/:id' => "money_transfer#show"
+      post 'money_transfers' => "money_transfer#create"
+      patch 'money_transfers/:id' => "money_transfer#update"
+      delete 'money_transfers/:id' => "money_transfer#destroy"
+    end
+  end
+  namespace :api do
+    namespace :v1 do
+      get 'users' => "user#index"
+      get 'users/:id' => "user#show"
+      post 'users' => "user#create"
+      patch 'users/:id' => "user#update"
+      delete 'users/:id' => "user#destroy"
+    end
+  end
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
