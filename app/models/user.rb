a4a23d30dbda4e9ac23c67eecd3536c8f2af48c7
Gerardo Tasistro <gtasistro@tiempometa.com>
2021-04-20 21:38:16 -0500

    basic working jwt auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,10 @@ class User < ApplicationRecord
   # STOCK AUTH SETTINGS
   # devise :database_authenticatable, :registerable,
   #       :recoverable, :rememberable, :validatable
+  #
+  include Devise::JWT::RevocationStrategies::JTIMatcher
 
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: self
   # alternate blacklist JWT Deny mechanism
   #:jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,19 @@ Rails.application.routes.draw do
       delete 'users/:id' => "user#destroy"
     end
   end
-  devise_for :users
+  #devise_for :users
+
+  devise_for :users,
+             defaults: { format: :json },
+             path: '',
+             path_names: {
+                 sign_in: 'api/login',
+                 sign_out: 'api/logout',
+                 registration: 'api/signup'
+             },
+             controllers: {
+                 sessions: 'sessions',
+                 registrations: 'registrations'
+             }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
